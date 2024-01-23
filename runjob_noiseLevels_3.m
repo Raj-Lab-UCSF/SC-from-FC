687c27eedaafe7ca2407621ada565bf3d3a9bb90
Farras Abdelnour <97462356+farrasnyc@users.noreply.github.com>
2024-01-23 10:45:14 -0500

    Add files via upload

---
+++ runjob_noiseLevels_3.m
@@ -0,0 +1,31 @@
+
+data_MICA = '/wynton/home/rajlab/fabdelnour/data/fMRI_DK';
+
+%data_MICA = '../../Data/fMRI_MICA'; % Data location
+
+numSubj = 5;
+parpool(numSubj);
+
+results_struct_noise = struct;
+
+sparcity = [0.01 0.2 0.5 1];
+
+jj=3;
+parfor subj = 1:numSubj
+	%for jj = 1:length(sparcity)
+    fprintf('Simulation %d/%d \n',subj,numSubj);
+    fprintf('Noise %d/%d \n',jj,length(sparcity));
+    [ outw, empiricalSC, x0_out, err, err2] = SC_fMRI_MICA_noise( subj , data_MICA , sparcity(jj) );
+
+    results_struct_noise(subj).outw = outw; 
+    results_struct_noise(subj).x0_out = x0_out; 
+    results_struct_noise(subj).err = err; 
+    results_struct_noise(subj).err2 = err2; 
+    results_struct_noise(subj).empiricalSC = empiricalSC;
+	%end
+	 
+end
+
+delete( gcp('nocreate') );
+
+save('results_struct_noise_3.mat' , 'results_struct_noise_3' );
